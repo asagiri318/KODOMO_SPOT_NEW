ccaaba5969b3b52a25a35140b2d1430e52c372fc
takahashi tatsuya <asagiri318z@gmail.com>
2024-07-26 17:15:53 +0900

    微調整

--- resources/views/spot/edit.blade.php
+++ resources/views/spot/edit.blade.php
@@ -73,7 +73,7 @@
             <div id="photo-fields" class="mb-4">
                 <label for="photos" class="block text-sm font-medium text-gray-700 mb-1">写真 (最大3つ)</label>
                 @foreach($spot->photos as $photo)
-                    <div class="photo-input flex items-center mb-2" data-photo-id="{{ $photo->id }}">
+                    <div class="photo-input container mx-auto flex items-center mb-2" data-photo-id="{{ $photo->id }}">
                         <input type="file" name="photos[]" class="mt-1 text-xs border border-gray-300 rounded-md shadow-sm">
                         <div class="flex items-center space-x-2 px-1">
                             <img src="{{ $photo->photo_path }}" class="px-1 h-10 w-16 rounded-lg">
