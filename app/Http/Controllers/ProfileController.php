581183335d24d5d84a5d67a6514462cc9964ceaa
takahashi tatsuya <asagiri318z@gmail.com>
2024-08-07 14:21:51 +0900

    微調整

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -52,12 +52,7 @@ class ProfileController extends Controller
 
             $file = $request->file('profile_photo');
             $filename = $user->id . '.' . $file->getClientOriginalExtension();
-
-            // 画像をリサイズ
-            $imageData = $this->resizeImage($file, 700, 400);
-            $path = 'profile_photos/' . $filename;
-            file_put_contents(storage_path('app/public/' . $path), $imageData);
-
+            $path = $file->storeAs('profile_photos', $filename, 'public');
             $user->photo = $path;
         }
 
@@ -101,24 +96,5 @@ class ProfileController extends Controller
         $children = $user->children()->get();
 
         return view('mypage', compact('user', 'spots', 'children'));
-    }
-
-    private function resizeImage($photo, $width, $height)
-    {
-        $img = imagecreatefromstring(file_get_contents($photo));
-        $originalWidth = imagesx($img);
-        $originalHeight = imagesy($img);
-
-        $resizedImage = imagecreatetruecolor($width, $height);
-        imagecopyresampled($resizedImage, $img, 0, 0, 0, 0, $width, $height, $originalWidth, $originalHeight);
-
-        ob_start();
-        imagejpeg($resizedImage);
-        $imageData = ob_get_clean();
-
-        imagedestroy($img);
-        imagedestroy($resizedImage);
-
-        return $imageData;
     }
 }
